a3efe87cec550c1420c581741f1a201aa06a0839
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2025-08-31 19:13:41 -0300

    feat: Adding the constructor and the validations in the models

--- backend/src/WordsCollection/WordsCollection/Models/UserModel.cs
+++ backend/src/WordsCollection/WordsCollection/Models/UserModel.cs
@@ -10,4 +10,21 @@ public class UserModel
     public List<WordModel> Words { get; private set; } =  new List<WordModel>();
     
     protected UserModel() { }
+
+    public UserModel(string emailAddress, string password)
+    {
+        Id = Guid.NewGuid();
+        SetEmail(emailAddress);
+        SetPassword(password);
+    }
+
+    public void SetEmail(string emailAddress)
+    {
+        Email = new EmailVo(emailAddress);
+    }
+
+    public void SetPassword(string password)
+    {
+        Password = new PasswordVo(password);
+    }
 }
--- backend/src/WordsCollection/WordsCollection/Models/WordModel.cs
+++ backend/src/WordsCollection/WordsCollection/Models/WordModel.cs
@@ -10,4 +10,46 @@ public class WordModel
     public UserModel User { get; private set; }
     
     protected WordModel() { }
+
+    public WordModel(string sentence, string meaning, UserModel user)
+    {
+        SetSentence(sentence);
+        SetMeaning(meaning);
+        CreatedAt = DateTime.Now;
+        User = user;
+    }
+
+    public void SetSentence(string sentence)
+    {
+        ValidateSentence(sentence);
+        Sentence = sentence;
+    }
+
+    private void ValidateSentence(string sentence)
+    {
+        switch (sentence.Length)
+        {
+            case < 4:
+                throw new ArgumentException("Sentence must have at least 4 characters.");
+            case > 255:
+                throw new ArgumentException("Sentence cannot exceed 255 characters.");
+        }
+    }
+
+    public void SetMeaning(string meaning)
+    {
+        ValidateMeaning(meaning);
+        Meaning = meaning;
+    }
+
+    private void ValidateMeaning(string meaning)
+    {
+        switch (meaning.Length)
+        {
+            case < 4:
+                throw new ArgumentException("Meaning must have at least 4 characters.");
+            case > 255:
+                throw new ArgumentException("Meaning cannot exceed 255 characters.");
+        }
+    }
 }
